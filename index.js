8a2b6a50f6a86a34c1a9dc4d8a108f17a3f2c6b3
David Shestopal <dshestopal@yahoo.com>
2020-04-08 12:48:44 -0700

    included server.js file and set up the server port

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code away!
+//included server.js file and set up the server port
+
+const server = require('./server.js');
+
+server.listen(5000, () => {
+  console.log('\n* Server Running on http://localhost:5000 *\n');
+});
